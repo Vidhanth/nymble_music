fe0f760199fc05dc5557eae0ebafcc7e5fe6fdee
Vidhanth <vidhanthbafna@gmail.com>
2024-03-21 18:08:26 +0530

    Work around issue with CanvasKit renderer

--- lib/presentation/screens/details_screen.dart
+++ lib/presentation/screens/details_screen.dart
@@ -35,136 +35,140 @@ class DetailsScreen extends StatelessWidget {
         ),
         extendBodyBehindAppBar: true,
         body: Container(
-          padding: const EdgeInsets.only(top: kBottomNavigationBarHeight + 20),
           decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
             image: DecorationImage(
-              opacity: 0.4,
               fit: BoxFit.cover,
               image: NetworkImage(
                 song.albumUrl,
               ),
             ),
           ),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 10,
-              sigmaY: 10,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  PhysicalModel(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(15),
-                    elevation: 10,
-                    child: Hero(
-                      tag: song.albumUrl,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: SizedBox(
-                          height: context.height * 0.45,
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: Image.network(
-                              song.albumUrl,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    song.name,
-                    style: montserratText.copyWith(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    song.artist,
-                    style: montserratText.copyWith(
-                      fontSize: 14.0,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20.0),
-                  BlocBuilder<DetailsBloc, DetailsState>(builder: (context, state) {
-                    if (state is! DetailsLoaded) {
-                      context.read<DetailsBloc>().add(
-                            PlayerSetupRequested(
-                              song.streamUrl,
-                              (progress) {
-                                context.read<DetailsBloc>().add(SongProgressUpdated(progress));
-                              },
-                            ),
-                          );
-                      return Center(
-                        child: CircularProgressIndicator(
-                          color: Theme.of(context).colorScheme.surfaceTint,
-                        ),
-                      );
-                    }
-
-                    return Center(
-                      child: Column(
-                        children: [
-                          AnimatedProgressBar(
-                            width: context.height * 0.5,
-                            value: state.progress,
-                            bgColor: Theme.of(context).colorScheme.surfaceTint.withOpacity(0.5),
-                            progressColor: Theme.of(context).colorScheme.surfaceTint,
-                            radius: BorderRadius.circular(20),
-                          ),
-                          const SizedBox(height: 16.0),
-                          IconButton(
-                            icon: SizedBox(
-                              height: 70,
-                              width: 70,
-                              child: CircleAvatar(
-                                backgroundColor: primaryColor,
-                                foregroundColor: Colors.white,
-                                child: AnimatedCrossFade(
-                                  firstChild: const Icon(Icons.play_arrow),
-                                  secondChild: const Icon(Icons.pause),
-                                  crossFadeState: state.isPlaying ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-                                  duration: 300.milliseconds,
+          child: Stack(
+            children: [
+              Container(
+                color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.6),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 10,
+                  sigmaY: 10,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, kBottomNavigationBarHeight + 20, 20, 20),
+                  child: Column(
+                    children: [
+                      PhysicalModel(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(15),
+                        elevation: 10,
+                        child: Hero(
+                          tag: song.albumUrl,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: SizedBox(
+                              height: context.height * 0.45,
+                              child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Image.network(
+                                  song.albumUrl,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
                             ),
-                            onPressed: () {
-                              context.read<DetailsBloc>().add(SongPlayPauseRequested());
-                            },
                           ),
-                        ],
+                        ),
                       ),
-                    );
-                  }),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  BlocBuilder<UserBloc, UserState>(
-                    builder: (context, state) {
-                      bool isFavorite = (state as UserLoaded).user.favorites.contains(song.id);
-                      return FavoritesButton(
-                        isFavorite: isFavorite,
-                        onTap: () {
-                          context.read<UserBloc>().add(UserFavoritesUpdateRequested(song.id));
+                      const Spacer(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        song.name,
+                        style: montserratText.copyWith(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        song.artist,
+                        style: montserratText.copyWith(
+                          fontSize: 14.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20.0),
+                      BlocBuilder<DetailsBloc, DetailsState>(builder: (context, state) {
+                        if (state is! DetailsLoaded) {
+                          context.read<DetailsBloc>().add(
+                                PlayerSetupRequested(
+                                  song.streamUrl,
+                                  (progress) {
+                                    context.read<DetailsBloc>().add(SongProgressUpdated(progress));
+                                  },
+                                ),
+                              );
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: Theme.of(context).colorScheme.surfaceTint,
+                            ),
+                          );
+                        }
+
+                        return Center(
+                          child: Column(
+                            children: [
+                              AnimatedProgressBar(
+                                width: context.height * 0.5,
+                                value: state.progress,
+                                bgColor: Theme.of(context).colorScheme.surfaceTint.withOpacity(0.5),
+                                progressColor: Theme.of(context).colorScheme.surfaceTint,
+                                radius: BorderRadius.circular(20),
+                              ),
+                              const SizedBox(height: 16.0),
+                              IconButton(
+                                icon: SizedBox(
+                                  height: 70,
+                                  width: 70,
+                                  child: CircleAvatar(
+                                    backgroundColor: primaryColor,
+                                    foregroundColor: Colors.white,
+                                    child: AnimatedCrossFade(
+                                      firstChild: const Icon(Icons.play_arrow),
+                                      secondChild: const Icon(Icons.pause),
+                                      crossFadeState: state.isPlaying ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                                      duration: 300.milliseconds,
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  context.read<DetailsBloc>().add(SongPlayPauseRequested());
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      BlocBuilder<UserBloc, UserState>(
+                        builder: (context, state) {
+                          bool isFavorite = (state as UserLoaded).user.favorites.contains(song.id);
+                          return FavoritesButton(
+                            isFavorite: isFavorite,
+                            onTap: () {
+                              context.read<UserBloc>().add(UserFavoritesUpdateRequested(song.id));
+                            },
+                          );
                         },
-                      );
-                    },
+                      ),
+                      const Spacer(),
+                    ],
                   ),
-                  const Spacer(),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
